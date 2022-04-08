f23a8e8932e42ca592f7e17a87d171c75d05f292
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 15:35:20 +0300

    Quiz Practice

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,6 @@ class Qpage extends StatefulWidget {
 
 class _QpageState extends State<Qpage> {
   List<Icon> scoreKeeper = [];
-  List<Question> questions = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
 
   int questionNumber = 0;
   @override
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,1 +1,9 @@
-class QuizBrain {}
+class QuizBrain {
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
+}
