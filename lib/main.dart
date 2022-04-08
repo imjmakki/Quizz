51c185d13f31e44bd90f408f61f55fb7dcfb9ce8
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 15:21:10 +0300

    Quiz Practice

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _QpageState extends State<Qpage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = questions[questionNumber].questionAnswer;
 
                 if (correctAnswer == false) {
                   print('You got the wrong answer!!');
