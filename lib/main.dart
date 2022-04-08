97dbe0d0cc1b7d661f838647ec4b633ad5bbeeab
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 13:21:18 +0300

    Quiz Practice

--- lib/main.dart
+++ lib/main.dart
@@ -103,3 +103,9 @@ class _QpageState extends State<Qpage> {
     );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
