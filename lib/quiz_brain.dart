190890ad48e63313918a5eb5619ed1cd09475cbe
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-08 19:17:40 +0300

    Quiz Practice

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,11 +1,15 @@
 import 'package:quizz/question.dart';
 
 class QuizBrain {
-  List<Question> questions = [
+  List<Question> _questions = [
     Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
     Question(
         q: 'Approximately one quarter of human bones are in the feet.',
         a: true),
     Question(q: 'A slug\'s blood is green.', a: true),
   ];
+
+  String getQuestionText(int questionNumber) {
+    return _questions[questionNumber].questionText;
+  }
 }
